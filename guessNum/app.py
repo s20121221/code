d2f654f9006f13e3c722a396887e55337683e805
s20121221 <truws98083@gmail.com>
2025-03-16 22:48:44 +0800

    變更

--- guessNum/app.py
+++ guessNum/app.py
@@ -2,7 +2,7 @@ import random
 
 answerNum = random.randint(1, 100)
 for i in range(1, 8):
-    guess = input("請在7次內猜出數字：")
+    guess = input(f"請在7次內猜出數字({i})：")
     if guess == "" or not guess.isdigit():
         break
     elif int(guess) == answerNum:
@@ -14,3 +14,4 @@ for i in range(1, 8):
         print("> 猜高些試試")
 print("\n正確答案是：" + str(answerNum))
 input()
+
